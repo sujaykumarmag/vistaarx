c084424e432931857ba992c532e64d1a4cad2eb7
sujaykumarmag <75253527+sujaykumarmag@users.noreply.github.com>
2021-10-10 11:18:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Network from "./components/Network/Network";
+import Header from "./components/Header";
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Network />
+        </div>
+    );
+}
+
+export default App;
